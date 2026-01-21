c8db6824c90654594a774e433968f3a365021ec6
Victory Olali <volali@ttu.edu>
2026-01-21 14:11:33 -0600

    Finish lab00

--- lab00/src/main.c
+++ lab00/src/main.c
@@ -4,9 +4,9 @@ int main(void) {
     printf("CS2413 Lab00 Starter Demo\n");
 
     // TODO: Replace the placeholder information below
-    printf("Name: YOUR_NAME_HERE\n");
-    printf("R#: ****XXXXX\n");
-    printf("Section: XXX\n");
+    printf("Name: Olali Victory\n");
+    printf("R#: R11788404\n");
+    printf("Section: 003\n");
     printf("Message: I can use GitHub for CS2413 labs!\n");
 
     return 0;
