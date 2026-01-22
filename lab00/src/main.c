09f545206568fa85b9cca22ad0e18cd6f729b2e6
Gadoyle <gadoyle@ttu.edu>
2026-01-22 10:02:55 -0600

    Update personal information in main.c

--- lab00/src/main.c
+++ lab00/src/main.c
@@ -4,11 +4,12 @@ int main(void) {
     printf("CS2413 Lab00 Starter Demo\n");
 
     // TODO: Replace the placeholder information below
-    printf("Name: YOUR_NAME_HERE\n");
-    printf("R#: ****XXXXX\n");
-    printf("Section: XXX\n");
+    printf("Name: Garrett_Doyle\n");
+    printf("R#: 11878897\n");
+    printf("Section: 506\n");
     printf("Message: I can use GitHub for CS2413 labs!\n");
 
     return 0;
 
 }
+
