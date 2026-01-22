f8d4c0af14c18a3633fd2fae05e0696de0c4be3f
Garrett Doyle <gadoyle@ttu.edu>
2026-01-22 10:35:48 -0600

    Finish lab00

--- lab00/src/main.c
+++ lab00/src/main.c
@@ -4,7 +4,7 @@ int main(void) {
     printf("CS2413 Lab00 Starter Demo\n");
 
     // TODO: Replace the placeholder information below
-    printf("Name: Garrett_Doyle\n");
+    printf("Name: Garrett Doyle\n");
     printf("R#: 11878897\n");
     printf("Section: 506\n");
     printf("Message: I can use GitHub for CS2413 labs!\n");
@@ -12,4 +12,3 @@ int main(void) {
     return 0;
 
 }
-
