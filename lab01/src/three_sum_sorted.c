80b04a16b417b30151b2941755c9ee6f65fb10e4
WONYOUNG-SONG <oldrock1999@gmail.com>
2026-02-03 08:41:40 -0600

    Lab01: Complete

--- lab01/src/three_sum_sorted.c
+++ lab01/src/three_sum_sorted.c
@@ -2,7 +2,24 @@
 
 int three_sum_zero_sorted_one(const int* nums, int n, int* out_i, int* out_j, int* out_k) {
     
-    // implement your code here
+    for (int i = 0; i < n - 2; i++){
+        int low = i + 1, high = n - 1;
+        while (low < high){
+            int sum = nums[i] + nums[low] + nums[high];
+            if (sum == 0){
+                *out_i = i;
+                *out_j = low;
+                *out_k = high;
+                return 1;
+            }
+            else if (sum > 0){
+                high--;
+            }
+            else {
+                low++;
+            }
+        }
+    }
 
     return 0;
 }
--- lab01/src/two_sum_sorted.c
+++ lab01/src/two_sum_sorted.c
@@ -3,8 +3,23 @@
 int two_sum_sorted(const int* nums, int n, int target, int* out_i, int* out_j) {
     if (!nums || !out_i || !out_j || n < 2) return 0;
 
-    // TODO: implement rwo sum solution for sorted arrays.
-
+    int low = 0, high = n - 1;
+    
+    while (low < high){
+        int sum = nums[low] + nums[high];
+        if (sum == target){
+            *out_i = low;
+            *out_j = high;
+            return 1;
+        }
+        else if (sum > target){
+            high--;
+        }
+        else {
+            low++;
+        }
+    }
+    
     return 0;
 }
 // think about the time complexity and space complexity of your solution
