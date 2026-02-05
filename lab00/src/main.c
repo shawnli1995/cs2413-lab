4a0cf22c7820ffd723e896b9008cb88af90371d7
laneyhood <laney.hood405@gmail.com>
2026-02-05 14:14:02 -0600

    First commit Lab00

--- lab00/src/main.c
+++ lab00/src/main.c
@@ -4,8 +4,8 @@ int main(void) {
     printf("CS2413 Lab00 Starter Demo\n");
 
     // TODO: Replace the placeholder information below
-    printf("Name: Shawn\n");
-    printf("R#: 11714613\n");
+    printf("Name: Laney\n");
+    printf("R#: 11934972\n");
     printf("Section: 002\n");
     printf("Message: I can use GitHub for CS2413 labs!\n");
 
