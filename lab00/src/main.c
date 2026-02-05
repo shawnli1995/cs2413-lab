e4577d9626090c65b607435953c0eae062e858a7
Ashley Nunez <ashley.nunez342@gmail.com>
2026-02-05 14:33:47 -0600

    Finish lab 00

--- lab00/src/main.c
+++ lab00/src/main.c
@@ -4,8 +4,8 @@ int main(void) {
     printf("CS2413 Lab00 Starter Demo\n");
 
     // TODO: Replace the placeholder information below
-    printf("Name: Shawn\n");
-    printf("R#: 11714613\n");
+    printf("Name: Ashley Nunez\n");
+    printf("R#: ****1247\n");
     printf("Section: 002\n");
     printf("Message: I can use GitHub for CS2413 labs!\n");
 
