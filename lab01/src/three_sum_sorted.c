835c03dd694de20b62f67d0f9116851e2f7754b3
Kolton Kopecky <longlord200@gmail.com>
2026-02-05 15:16:16 -0600

    Finished Lab 1

--- lab01/src/three_sum_sorted.c
+++ lab01/src/three_sum_sorted.c
@@ -1,8 +1,28 @@
 #include "three_sum_sorted.h"
 
 int three_sum_zero_sorted_one(const int* nums, int n, int* out_i, int* out_j, int* out_k) {
-    
-    // implement your code here
+    if (!nums || n < 3) return 0;
 
+    for (int i = 0; i < n - 2; i++) {
+        int l = i + 1; // Left pointer starts after i
+        int r = n - 1; // Right pointer starts at the end
+        
+        // We need: nums[l] + nums[r] == 0 - nums[i]
+        int target = -nums[i];
+
+        while (l < r) {
+            int sum = nums[l] + nums[r];
+
+            if (sum == target) {
+                *out_i = i;
+                *out_j = l;
+                *out_k = r;
+                return 1; // Found valid triplet
+            }
+
+            if (sum < target) l++;
+            else r--;
+        }
+    }
     return 0;
 }
--- lab01/src/two_sum_sorted.c
+++ lab01/src/two_sum_sorted.c
@@ -1,11 +1,24 @@
 #include "two_sum_sorted.h"
 
 int two_sum_sorted(const int* nums, int n, int target, int* out_i, int* out_j) {
-    if (!nums || !out_i || !out_j || n < 2) return 0;
+    if (!nums || n < 2) return 0;
 
-    // TODO: implement rwo sum solution for sorted arrays.
+    int l = 0;      // Left pointer
+    int r = n - 1;  // Right pointer
 
+    while (l < r) {
+        int sum = nums[l] + nums[r];
+
+        if (sum == target) {
+            *out_i = l;
+            *out_j = r;
+            return 1;
+        }
+        
+        // If sum is too small, increment left to get a larger value.
+        // If sum is too big, decrement right to get a smaller value.
+        if (sum < target) l++;
+        else r--;
+    }
     return 0;
 }
-// think about the time complexity and space complexity of your solution
-
