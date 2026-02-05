e9bb6ce94b260d8d4e5a9a8fa0895a75cca9d531
Ashley Nunez <ashley.nunez342@gmail.com>
2026-02-05 14:31:16 -0600

    fix

---
+++ lab00/src/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("CS2413 Lab00 Starter Demo\n");
+
+    // TODO: Replace the placeholder information below
+    printf("Name: Shawn\n");
+    printf("R#: 11714613\n");
+    printf("Section: 002\n");
+    printf("Message: I can use GitHub for CS2413 labs!\n");
+
+    return 0;
+
+}
