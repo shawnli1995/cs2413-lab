2387aa10051a7319def287880ee504738d3f6ae6
Ashley Nunez <ashley.nunez342@gmail.com>
2026-02-05 15:05:33 -0600

    Finish lab 01 extras

--- lab01/src/three_sum_sorted.c
+++ lab01/src/three_sum_sorted.c
@@ -3,6 +3,18 @@
 int three_sum_zero_sorted_one(const int* nums, int n, int* out_i, int* out_j, int* out_k) {
     
     // implement your code here
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            for(int k = 0; k<n; k++){
+                if(nums[i] + nums[j] + nums[k] == 0 && i<j && j<k){
+                    *out_k = k;
+                    *out_i = i;
+                    *out_j = j;
+                    return 1;
+                }
+            }
+        }
+    }
 
     return 0;
 }
--- lab01/src/two_sum_sorted.c
+++ lab01/src/two_sum_sorted.c
@@ -4,6 +4,20 @@ int two_sum_sorted(const int* nums, int n, int target, int* out_i, int* out_j) {
     if (!nums || !out_i || !out_j || n < 2) return 0;
 
     // TODO: implement rwo sum solution for sorted arrays.
+    for(int i = 0; i<n;){
+        for(int j = n-1; j<n;){
+            if (nums[i] + nums[j]<target)
+                i++;
+            else if(nums[i] + nums[j] > target)
+                j--;
+            else{
+                *out_i = i;
+                *out_j = j;
+                return 1;
+            }
+        }
+
+    }
 
     return 0;
 }
