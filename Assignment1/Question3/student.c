3561ae2c7bdfd1aeec7ea7f98e6ccc19a78943ec
NickDitmore <nickditmore@outlook.com>
2026-02-20 15:11:17 -0600

    Add files via upload

--- Assignment1/Question3/student.c
+++ Assignment1/Question3/student.c
@@ -26,7 +26,25 @@
 //Output parameter (returnSize): set *returnSize to the number of digits in the returned array.
 
 int* plusOne(int* digits, int digitsSize, int* returnSize) {
-    // TODO: implement
+    for (int i = digitsSize - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;              // no carry needed
+            *returnSize = digitsSize;
+            return digits;
+        }
+        digits[i] = 0;                // carry over
+    }
+
+    // If we get here, all digits were 9 (e.g., 999 -> 1000)
+    int* result = (int*)malloc((digitsSize + 1) * sizeof(int));
+    result[0] = 1;
+
+    for (int i = 1; i <= digitsSize; i++) {
+        result[i] = 0;
+    }
+
+    *returnSize = digitsSize + 1;
+    return result;
 
     
 }
