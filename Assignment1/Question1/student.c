cf82caa29dc659d4be55da41c775f472c7cf78d7
NickDitmore <nickditmore@outlook.com>
2026-02-20 14:53:51 -0600

    Add files via upload

--- Assignment1/Question1/student.c
+++ Assignment1/Question1/student.c
@@ -15,7 +15,28 @@
 
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
 
- // TODO: implement
+    int i = m - 1;          // last real element in nums1
+    int j = n - 1;          // last element in nums2
+    int k = m + n - 1;      // last position in nums1
+
+    // Merge from the back
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        k--;
+    }
+
+    // If nums2 still has elements, copy them
+    while (j >= 0) {
+        nums1[k] = nums2[j];
+        j--;
+        k--;
+    }
 
 
 
