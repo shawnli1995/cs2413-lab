63217c2f3e1ad895360678018e36f34425e692df
ozjahir <jahirzepeda16@icloud.com>
2026-02-20 21:12:09 -0600

    student.c

--- Assignment1/Question1/student.c
+++ Assignment1/Question1/student.c
@@ -16,7 +16,23 @@
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
 
  // TODO: implement
+(void)nums1Size;
+ (void)nums2Size;
+    int i = m - 1; 
+    int j = n - 1;
+    int k = m + n -1;
+while (j>=0){
 
+    if (nums1[i]>= nums2[j]){
+        nums1[k]= nums1[i];
+        k=k-1;
+        i =i-1;
+    } 
+    else {
+        nums1[k]=nums2[j];
+        k=k-1;
+        j=j-1;
+    } }
 
 
 }
