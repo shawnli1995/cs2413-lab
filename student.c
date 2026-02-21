892866b2a3456392adad3ef30b8eb402102b7f8f
ozjahir <jahirzepeda16@icloud.com>
2026-02-20 20:47:59 -0600

    Add files via upload

--- student.c
+++ student.c
@@ -1,45 +1,39 @@
 #include "Student.h"
 
-//You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n:
-//nums1 has length m + n
-//The first m values in nums1 are real elements
-//The last n values in nums1 are placeholders (0)
-//nums2 has length n
-//Goal: Merge nums2 into nums1 so that nums1 becomes one sorted array.
-//Return: Nothing (modify nums1 in-place).
+//Given an array nums of size n, return the majority element.
 
-//Example:
-//Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
-//Output: nums1 = [1,2,2,3,5,6]
-//Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+//The majority element appears more than ⌊n/2⌋ times.
+//You may assume the majority element always exists in the array.
 
-void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
+//Example 1:
+//Input: nums = [3,2,3]
+//Output: 3
 
- // TODO: implement
- (void)nums1Size;
- (void)nums2Size;
-    int i = m - 1; 
-    int j = n - 1;
-    int k = m + n -1;
-while (j>=0){
-
-    if (nums1[i]>= nums2[j]){
-        nums1[k]= nums1[i];
-        k=k-1;
-        i =i-1;
-    } 
-    else {
-        nums1[k]=nums2[j];
-        k=k-1;
-        j=j-1;
-    } }
+//Example 2:
+//Input: nums = [2,2,1,1,1,2,2]
+//Output: 2
 
 
+int majorityElement(int* nums, int numsSize) {
+     // TODO: implement
+     int count = 0;
+     int candidate = nums[0];
 
-
-    
-    
-
+     for(int i=0; i < numsSize; i++){
+         if (count == 0)
+         {
+          candidate = nums[i];
+          count ++;
+         } else if (nums[i] == candidate)
+         {
+          count ++;
+         } else {
+          count --;
+         }
+         
+         
+     }
+     return candidate;
 
 
 }
