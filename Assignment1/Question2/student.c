4d259c0bf57b1d973451fc5274afbf0407f9cb9a
ozjahir <jahirzepeda16@icloud.com>
2026-02-20 21:13:24 -0600

    student.c

--- Assignment1/Question2/student.c
+++ Assignment1/Question2/student.c
@@ -16,6 +16,24 @@
 
 int majorityElement(int* nums, int numsSize) {
      // TODO: implement
+int count = 0;
+     int candidate = nums[0];
+
+     for(int i=0; i < numsSize; i++){
+         if (count == 0)
+         {
+          candidate = nums[i];
+          count ++;
+         } else if (nums[i] == candidate)
+         {
+          count ++;
+         } else {
+          count --;
+         }
+         
+         
+     }
+     return candidate;
 
 
 
